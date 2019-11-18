0a5106b429e1f6abaa35e0300a6c3f1bea6f627b
Jrogerson74 <jrsquish@gmail.com>
2019-11-18 11:26:00 -0700

    blogs

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem() {
+    axios.get(`https://jamierogerson.devcamp.space/portfolio/portfolio_blogs/${this.state
+      .currentId}`
+    )
+    .then(response => {
+      console.log("response", response);
+    })
+    .catch(error => {
+      console.log("getBlogItem error", error);
+    });
+  }
+
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog Detail</h1>
+      </div>
+    );
+  }
+}
