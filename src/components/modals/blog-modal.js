54a76ae4de0d9a1fa69517af4fd2ce1a31656eb9
Jrogerson74 <jrsquish@gmail.com>
2019-11-26 09:50:46 -0700

    modal updated

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
 
-export default class BlogModal  extends Component {
+import BlogForm from "../blog/blog-form";
+
+ReactModal.setAppElement(".app-wrapper");
+
+export default class BlogModal extends Component {
   constructor(props) {
     super(props);
+
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%",
+        width: "800px"
+      },
+      overlay: {
+        backgroundColor: "rgba(1, 1, 1, 0.75)"
+      }
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
   }
+
+ handleSuccessfulFormSubmission(blog) {
+   console.log("blog from blog form", blog);
+ }
 
   render() {
     return (
-      <ReactModal isOpen={this.props.modalIsOpen}>
-        <h1>I'm in a Modal</h1>
+      <ReactModal
+        style={this.customStyles}
+        onRequestClose={() => {
+          this.props.handleModalClose();
+        }}
+        isOpen={this.props.modalIsOpen}
+    >
+        <BlogForm
+          handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+        />
       </ReactModal>
     );
   }
